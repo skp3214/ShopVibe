0602cc44303374a19813f3e72e9cc65c498f6c94
Sachin Prajapati <skprajapati3214@gmail.com>
2024-11-01 17:30:48 +0530

    shipping page code

---
+++ frontend/src/pages/shipping.tsx
@@ -0,0 +1,39 @@
+import { ChangeEvent, useState } from "react"
+import { BiArrowBack } from "react-icons/bi"
+import { useNavigate } from "react-router-dom"
+
+const Shipping = () => {
+    const navigate=useNavigate();
+    const [shippingInfo, setShippingInfo] = useState({
+        address: "",
+        city: "",
+        country: "",
+        state: "",
+        pinCode: "",
+    })
+    const changeHandler = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        setShippingInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+    }
+    return (
+        <div className="shipping">
+            <button className="back-btn" onClick={() => navigate(-1)}><BiArrowBack /></button>
+            <form>
+                <h1>Shipping Address</h1>
+                <input type="text" name="address" placeholder="Enter Your Address" onChange={changeHandler} value={shippingInfo.address} />
+                <input type="text" name="city" placeholder="Enter Your City" onChange={changeHandler} value={shippingInfo.city} />
+                <input type="text" name="state" placeholder="Enter Your State" onChange={changeHandler} value={shippingInfo.state} />
+                <select name="country" required value={shippingInfo.country} onChange={changeHandler}>
+                    <option value="">Select Your Country</option>
+                    <option value="USA">USA</option>
+                    <option value="India">India</option>
+                    <option value="UK">UK</option>
+                    <option value="Australia">Australia</option>
+                </select>
+                <input type="number" name="pinCode" placeholder="Enter Your PinCode" onChange={changeHandler} value={shippingInfo.pinCode} />
+                <button type="submit">Pay Now</button>
+            </form>
+        </div>
+    )
+}
+
+export default Shipping

a52294c7bb0d335fec27d3e2282ac4319087950f
Sachin Prajapati <skprajapati3214@gmail.com>
2024-11-02 20:19:45 +0530

    user api routes

---
+++ backend/src/routes/user.routes.ts
@@ -0,0 +1,11 @@
+// FILE: routes/user.routes.ts
+import express from 'express';
+const router = express.Router();
+import { newUser,getAllUsers,getUser,deleteUser } from '../controllers/user.controllers.js';
+import { adminOnly } from '../middleware/auth.js';
+
+router.post('/new', newUser);
+router.get('/all',adminOnly ,getAllUsers);
+router.route('/:id').get(getUser).delete(adminOnly,deleteUser);
+
+export default router;

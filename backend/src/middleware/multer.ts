01d0c4d9cc44c32bcf20c60d0ae3f6437c660284
Sachin Prajapati <skprajapati3214@gmail.com>
2024-11-03 23:32:28 +0530

    Enhance multer middleware to generate unique filenames for uploaded files

--- backend/src/middleware/multer.ts
+++ backend/src/middleware/multer.ts
@@ -1,11 +1,14 @@
 import multer from "multer";
-
+import { v4 as uuid } from "uuid";
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "uploads");
     },
     filename: (req, file, cb) => {
-        cb(null, file.originalname);
+        const id=uuid();
+        const extName = file.originalname.split(".").pop();
+        const fileName = `${id}.${extName}`;
+        cb(null, fileName);
     }
 })
 

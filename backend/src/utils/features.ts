9b0abec36c7ae62c474f887507811f359ae8eca5
Sachin Prajapati <skprajapati3214@gmail.com>
2024-11-04 00:15:09 +0530

    Add cache invalidation utility for product updates

--- backend/src/utils/features.ts
+++ backend/src/utils/features.ts
@@ -1,4 +1,7 @@
 import mongoose from "mongoose"
+import { InvalidatesCacheType } from "../types/types.js";
+import { Product } from "../models/product.js";
+import { myCache } from "../app.js";
 
 export const connectDB = () => {
     mongoose.connect("mongodb+srv://skprajapati3214:Sachin3214@backend-cluster.qfpxr0l.mongodb.net/?retryWrites=true&w=majority", {
@@ -8,4 +11,23 @@ export const connectDB = () => {
     }).catch((err) => {
         console.log(err);
     })
+}
+
+export const invalidatesCache = async({
+    product,
+    order,
+    admin
+}:InvalidatesCacheType) => {
+    if(product){
+        const productKeys:string[] = [
+            "latestProducts",
+            "categories",
+            "adminProducts",
+        ];
+        const productsId=await Product.find().select("_id");
+        productsId.forEach((product)=>{
+            productKeys.push(`product-${product._id}`);
+        });
+        myCache.del(productKeys);
+    }
 }

2dd2b4c3fabeca54fb7fa80328d549111062c93f
Sachin Prajapati <skprajapati3214@gmail.com>
2024-11-03 23:34:58 +0530

    Add product routes and caching to the application

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,19 @@
 import express, { NextFunction, Request, Response } from 'express';
 import userRoutes from './routes/user.routes.js';
+import productRoutes from './routes/product.routes.js';
 import { connectDB } from './utils/features.js';
 import { errorMiddleWare } from './middleware/error.js';
+import NodeCache from 'node-cache';
 connectDB();
+export const myCache = new NodeCache();
 const app = express();
 app.use(express.json());
 app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/product", productRoutes);  
 app.get('/', (req, res) => { 
     res.send('Hello World!'); 
 });
+app.use('/uploads', express.static('uploads'));
 app.use(errorMiddleWare);
 app.listen(3000, () => {
     console.log('Server is running on port 3000');

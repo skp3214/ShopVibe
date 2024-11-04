b7319935dbb4bbfb465edb81cfcff6281827ec03
Sachin Prajapati <skprajapati3214@gmail.com>
2024-11-04 23:29:52 +0530

    Add order routes and improve environment variable handling in app setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,41 @@
 import express, { NextFunction, Request, Response } from 'express';
 import userRoutes from './routes/user.routes.js';
 import productRoutes from './routes/product.routes.js';
+import orderRoutes from './routes/order.routes.js';
 import { connectDB } from './utils/features.js';
 import { errorMiddleWare } from './middleware/error.js';
 import NodeCache from 'node-cache';
-connectDB();
+import {config} from 'dotenv';
+import morgan from 'morgan';
+
+config(
+    {
+        path:"./.env"
+    }
+);
+
+const MONGO_URI=process.env.MONGO_URI||"";
+connectDB(MONGO_URI as string);
 export const myCache = new NodeCache();
+
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
+
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/product", productRoutes);  
+app.use("/api/v1/order", orderRoutes);
+
+
 app.get('/', (req, res) => { 
     res.send('Hello World!'); 
 });
 app.use('/uploads', express.static('uploads'));
+
 app.use(errorMiddleWare);
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+
+const PORT=process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ',`${PORT}`);
 });

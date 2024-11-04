07f06ea3be9491b1e57ca65663efb45c0f05d2c0
Sachin Prajapati <skprajapati3214@gmail.com>
2024-11-05 04:05:11 +0530

    Add order processing and deletion routes to order management

--- backend/src/routes/order.routes.ts
+++ backend/src/routes/order.routes.ts
@@ -1,6 +1,5 @@
-import exp from 'constants';
 import express from 'express';
-import { AdminOrders, getSingleOrder, MyOrders, newOrder } from '../controllers/order.controller.js';
+import { AdminOrders, deleteOrder, getSingleOrder, MyOrders, newOrder, processOrder } from '../controllers/order.controller.js';
 import { adminOnly } from '../middleware/auth.js';
 
 const router = express.Router();
@@ -8,6 +7,6 @@ const router = express.Router();
 router.post('/new',newOrder);
 router.get('/my',MyOrders);
 router.get('/all',adminOnly,AdminOrders);
-router.route('/:id').get(getSingleOrder)
+router.route('/:id').get(getSingleOrder).put(adminOnly,processOrder).delete(adminOnly,deleteOrder);
 
 export default router;

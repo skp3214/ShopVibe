86a2f858e468f4dea638e170ca7b1c2d6768008d
Sachin Prajapati <skprajapati3214@gmail.com>
2024-11-20 23:49:17 +0530

    Added createPaymentIntent route for payment processing

--- backend/src/routes/payment.routes.ts
+++ backend/src/routes/payment.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
 import { adminOnly } from '../middleware/auth.js';
-import { allCoupons, applyDiscount, deleteCoupons, newCoupon } from '../controllers/payment.controller.js';
+import { allCoupons, applyDiscount, createPaymentIntent, deleteCoupons, newCoupon } from '../controllers/payment.controller.js';
 
 const router=express.Router();
 
+router.post("/create",createPaymentIntent)
 router.post("/coupon/new",adminOnly,newCoupon);
 router.get("/discount",applyDiscount);
 router.get("/coupon/all",adminOnly,allCoupons);

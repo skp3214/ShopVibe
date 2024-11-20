1076657434820d593ea932ccf6362bbee8320c79
Sachin Prajapati <skprajapati3214@gmail.com>
2024-11-20 23:49:02 +0530

    Added Stripe integration and configure environment variable for Stripe key

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,6 +9,7 @@ import { errorMiddleWare } from './middleware/error.js';
 import NodeCache from 'node-cache';
 import {config} from 'dotenv';
 import morgan from 'morgan';
+import Stripe from 'stripe';
 
 config(
     {
@@ -17,7 +18,9 @@ config(
 );
 
 const MONGO_URI=process.env.MONGO_URI||"";
+const StripeKey=process.env.STRIPE_KEY||"";
 connectDB(MONGO_URI as string);
+export const stripe=new Stripe(StripeKey)
 export const myCache = new NodeCache();
 
 const app = express();

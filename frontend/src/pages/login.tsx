1ffc670846097bc35c7f2f3d5a434cf7ee63d870
Sachin Prajapati <skprajapati3214@gmail.com>
2024-12-01 15:58:23 +0530

    Implemented Google sign-in functionality with user data handling and notifications

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,9 +1,42 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 import { useState } from "react"
+import toast from "react-hot-toast"
 import { FcGoogle } from "react-icons/fc"
+import { auth } from "../firebase"
+import { useLoginMutation } from "../redux/api/UserAPI"
+import { FetchBaseQueryError } from "@reduxjs/toolkit/query"
+import { MessageResponse } from "../types/apitypes"
 
 const Login = () => {
     const [gender, setGender] = useState("")
     const [date, setDate] = useState("")
+    const [login]=useLoginMutation()
+    const loginHandler=async()=>{
+        try {
+            const provider = new GoogleAuthProvider();
+            const {user}=await signInWithPopup(auth, provider);
+            console.log(user);
+            const res=await login({
+                name:user.displayName!,
+                email:user.email!,
+                photo:user.photoURL!,
+                gender,
+                dob:date,
+                role:"user",
+                _id:user.uid
+            })
+            if("data" in res){
+                toast.success(res.data?.message as string);
+            }
+            else{
+                const error=res.error as FetchBaseQueryError;
+                const message=(error.data as MessageResponse).message;
+                toast.error(message);
+            }
+        } catch (error) {
+            toast.error("Sign in failed")
+        }
+    }
     return (
         <div className="login">
             <main>
@@ -29,7 +62,7 @@ const Login = () => {
 
                 <div>
                     <p>Already Signed In Once</p>
-                    <button >
+                    <button onClick={loginHandler}>
                         <FcGoogle /> <span>Sign in with Google</span>
                     </button>
                 </div>

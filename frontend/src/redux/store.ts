1039bd75ba1a9c3159de7a40fda6614229c52b27
Sachin Prajapati <skprajapati3214@gmail.com>
2024-12-02 23:31:48 +0530

    Added UserReducer to Redux store configuration

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { UserApi } from "./api/UserAPI";
+import { UserReducer } from "./reducer/UserReducer";
 
 export const server=import.meta.env.VITE_SERVER_URL
 export const store=configureStore({
     reducer:{
-        [UserApi.reducerPath]:UserApi.reducer
+        [UserApi.reducerPath]:UserApi.reducer,
+        [UserReducer.name]:UserReducer.reducer
     },
     middleware:(getDefaultMiddleware)=>getDefaultMiddleware()
                                       .concat(UserApi.middleware)

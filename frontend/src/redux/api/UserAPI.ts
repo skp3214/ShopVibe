6cf1bef632d0ebb6ac367d02357cb5d6bd77cc62
Sachin Prajapati <skprajapati3214@gmail.com>
2024-12-02 23:32:01 +0530

    Added getUser function to UserAPI for fetching user data using axios

--- frontend/src/redux/api/UserAPI.ts
+++ frontend/src/redux/api/UserAPI.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { MessageResponse } from '../../types/apitypes';
+import { MessageResponse, UserResponse } from '../../types/api.types';
 import { User } from '../../types/types';
+import axios from 'axios';
 
 export const UserApi = createApi({
   reducerPath: 'userApi',
@@ -16,4 +17,12 @@ export const UserApi = createApi({
   }),
 });
 
+export const getUser=async(id:string)=>{
+  try {
+    const {data}:{data:UserResponse}=await axios.get(`${import.meta.env.VITE_SERVER_URL}/api/v1/user/${id}`);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
 export const { useLoginMutation } = UserApi;

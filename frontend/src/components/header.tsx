d9d1cf4f89292d4dce84ddaf77abf96f1a65b735
Sachin Prajapati <skprajapati3214@gmail.com>
2024-12-03 21:03:06 +0530

    Implemented logout functionality in Header component with error handling and success notifications

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -2,13 +2,22 @@ import { useState } from "react"
 import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa"
 import { Link } from "react-router-dom"
 import { User } from "../types/types"
-interface PropsType{
-    user:User|null
+import { signOut } from "firebase/auth"
+import { auth } from "../firebase"
+import toast from "react-hot-toast"
+interface PropsType {
+    user: User | null
 }
-const Header = ({user}:PropsType) => {
+const Header = ({ user }: PropsType) => {
     const [isOpen, setIsOpen] = useState<boolean>(false)
-    const logoutHandler = () => {
-        setIsOpen(false)
+    const logoutHandler = async () => {
+        try {
+            await signOut(auth);
+            toast.success("Signout out successfully")
+            setIsOpen(false)
+        } catch (error) {
+            toast.error("Sign Out Failed");
+        }
     }
     return (
         <nav className="header">

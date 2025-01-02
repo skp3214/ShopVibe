4d63793ddce3ed261ef0de3e33761c025fd39ece
Sachin Prajapati <skprajapati3214@gmail.com>
2025-01-02 11:16:59 +0530

    Add CartReducer for managing cart state and operations

---
+++ frontend/src/redux/reducer/CartReducer.ts
@@ -0,0 +1,51 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { CartReducerIntialState } from "../../types/reducer.types";
+import { CartItem } from "../../types/types";
+
+const initialState: CartReducerIntialState = {
+    loading: false,
+    cartItems: [],
+    subtotal: 0,
+    tax: 0,
+    shippingCharges: 0,
+    discount: 0,
+    total: 0,
+    shippingInfo: {
+        address: '',
+        city: '',
+        state: '',
+        pinCode: '',
+        country: '',
+    }
+}
+export const CartReducer = createSlice({
+    name: 'cartReducer',
+    initialState,
+    reducers: {
+        addToCart: (state, action: PayloadAction<CartItem>) => {
+            state.loading = true;
+            const index = state.cartItems.findIndex((item) => item.productID === action.payload.productID);
+            if (index !== -1) {
+                state.cartItems[index] = action.payload;
+                state.loading = false;
+                return;
+            }
+            state.cartItems.push(action.payload);
+            state.loading = false;
+        },
+        removeFromCart: (state, action: PayloadAction<string>) => {
+            state.loading = true;
+            state.cartItems = state.cartItems.filter((item) => item.productID !== action.payload);
+            state.loading = false;
+        },
+        calculatePrice:(state)=>{
+            const subtotal=state.cartItems.reduce((acc,item)=>acc+item.price*item.quantity,0);
+            state.subtotal=subtotal;
+            state.tax=Math.round(state.subtotal*0.18);
+            state.shippingCharges=state.subtotal>1000?0:200;
+            state.total=state.subtotal+state.tax+state.shippingCharges-state.discount;
+        }
+    }
+})
+
+export const { addToCart, removeFromCart,calculatePrice } = CartReducer.actions;

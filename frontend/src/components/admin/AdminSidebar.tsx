7689e29e2f03fa139db9fe78cc79b9bf75d2a7ac
Sachin Prajapati <skprajapati3214@gmail.com>
2025-01-16 02:33:56 +0530

    Update AdminSidebar to change coupon link to discount and use new icon

--- frontend/src/components/admin/AdminSidebar.tsx
+++ frontend/src/components/admin/AdminSidebar.tsx
@@ -16,6 +16,7 @@ import {
 } from "react-icons/ri";
 import { Link, Location, useLocation } from "react-router-dom";
 import { IconType } from "react-icons";
+import { MdDiscount } from "react-icons/md";
 
 const AdminSidebar = () => {
   const location = useLocation();
@@ -102,13 +103,13 @@ const DivOne = ({ location }: { location: Location }) => (
         Icon={AiFillFileText}
         location={location}
       />
+
       <Li
-        url="/admin/coupon" 
-        text="Coupon"
-        Icon={RiCoupon3Fill}
+        url="/admin/discount"
+        text="Discount"
+        Icon={MdDiscount}
         location={location}
       />
-
     </ul>
   </div>
 );

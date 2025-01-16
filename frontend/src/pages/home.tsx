8645658fde0acf1a7f1977c4d9fd594229c3ac86
Sachin Prajapati <skprajapati3214@gmail.com>
2025-01-16 23:51:54 +0530

    Enhance home page with category listing, video cover, and service features

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,50 +1,204 @@
-import { Link } from "react-router-dom"
-import ProductCard from "../components/product-card.tsx"
-import { useLatestProductsQuery } from "../redux/api/ProductAPI.ts";
 import toast from "react-hot-toast";
-import { SkeletonLoader } from "../components/loader.tsx";
-import { CartItem } from "../types/types.ts";
 import { useDispatch } from "react-redux";
-import { addToCart } from "../redux/reducer/CartReducer.ts";
+import { Link } from "react-router-dom";
+import { SkeletonLoader } from "../components/loader";
+import ProductCard from "../components/product-card";
+import { CartItem } from "../types/types";
+import videoCover from "../assets/videos/cover.mp4";
+import { FaAnglesDown, FaHeadset } from "react-icons/fa6";
+import { motion } from "framer-motion";
+import { Slider } from "6pp";
+import { TbTruckDelivery } from "react-icons/tb";
+import { LuShieldCheck } from "react-icons/lu";
+import { useCategoriesQuery, useLatestProductsQuery } from "../redux/api/ProductAPI";
+import { addToCart } from "../redux/reducer/CartReducer";
+import { useEffect } from "react";
+import { CustomError } from "../types/api.types";
+
+const banners = [
+  "https://res.cloudinary.com/dj5q966nb/image/upload/v1719253445/rmbjpuzctjdbtt8hewaz.png",
+  "https://res.cloudinary.com/dj5q966nb/image/upload/v1719253433/ticeufjqvf6napjhdiee.png",
+];
+
+
+
+const services = [
+  {
+    icon: <TbTruckDelivery />,
+    title: "FREE AND FAST DELIVERY",
+    description: "Free delivery for all orders over $200",
+  },
+  {
+    icon: <LuShieldCheck />,
+    title: "SECURE PAYMENT",
+    description: "100% secure payment",
+  },
+  {
+    icon: <FaHeadset />,
+    title: "24/7 SUPPORT",
+    description: "Get support 24/7",
+  },
+];
 
 const Home = () => {
-  const { data, isLoading, isError } = useLatestProductsQuery("");
-  const dispatch = useDispatch();
-  const addToCartHandler = (cartItem:CartItem) => {
-    if(cartItem.stock<=0){
-      return toast.error("Out of stock")
-    }
-    dispatch(addToCart(cartItem))
-    toast.success("Added to cart")
-  };
-  if (isLoading) return <>
-    <SkeletonLoader width="" />
-  </>
-  if (isError) toast.error("Failed to fetch products");
-  return (
-    <div className="home">
-      <section></section>
-      <h1>
-        Latest Products
-        <Link to={"/search"} className="findmore">More</Link>
-      </h1>
-      <main>
-        {
-          data?.products.map((product) => (
-            <ProductCard
-              key={product._id}
-              productId={product._id}
-              photos={product.photos}
-              name={product.name}
-              price={product.price}
-              stock={product.stock}
-              handler={addToCartHandler}
-            />
-          ))
-        }
-      </main>
-    </div>
-  )
-}
+  const { data, isError, isLoading } = useLatestProductsQuery("");
 
-export default Home
+  const { data:categoryData, isLoading:isCategoryLoading, isError:isCategoryError, error:categoryError } = useCategoriesQuery("");
+  useEffect(() => {
+    if (isCategoryError) {
+      toast.error((categoryError as CustomError)?.data?.message || "An error occurred");
+    }
+  }, [isCategoryError, categoryError]);
+
+  const categories = categoryData?.categories || [];
+  const dispatch = useDispatch();
+
+  const addToCartHandler = (cartItem: CartItem) => {
+    if (cartItem.stock < 1) return toast.error("Out of Stock");
+    dispatch(addToCart(cartItem));
+    toast.success("Added to cart");
+  };
+
+  if (isError) toast.error("Cannot Fetch the Products");
+
+  const coverMessage =
+    "Fashion isn't just clothes; it's a vibrant language. Silhouettes and textures speak volumes, a conversation starter with every bold print. It's a way to tell our story, a confidence booster, or a playful exploration. From elegance to rebellion, fashion lets us navigate the world in style.".split(
+      " "
+    );
+
+  return (
+    <>
+      <div className="home">
+        <section></section>
+
+        <div>
+          <aside>
+            <h1>Categories</h1>
+            <ul>
+              {
+                isCategoryLoading ? <SkeletonLoader width="100%" /> : (
+                  categories.map((category) => (
+                    <li key={category}>
+                      <Link to={`/search?category=${category}`}>{category.toUpperCase()}</Link>
+                    </li>
+                  ))
+                )
+              }
+            </ul>
+          </aside>
+          <Slider
+            autoplay
+            autoplayDuration={1500}
+            showNav={false}
+            images={banners}
+          />
+        </div>
+
+        <h1>
+          Latest Products
+          <Link to="/search" className="findmore">
+            More
+          </Link>
+        </h1>
+
+        <main>
+          {isLoading ? (
+            <>
+              {Array.from({ length: 6 }, (_, i) => (
+                <div key={i} style={{ height: "25rem" }}>
+                  <SkeletonLoader width="" />
+                  <SkeletonLoader width="" />
+                  <SkeletonLoader width="" />
+                </div>
+              ))}
+            </>
+          ) : (
+            data?.products.map((i) => (
+              <ProductCard
+                key={i._id}
+                productId={i._id}
+                name={i.name}
+                price={i.price}
+                stock={i.stock}
+                handler={addToCartHandler}
+                photos={i.photos}
+              />
+            ))
+          )}
+        </main>
+      </div>
+
+      <article className="cover-video-container">
+        <div className="cover-video-overlay"></div>
+        <video autoPlay loop muted src={videoCover} />
+        <div className="cover-video-content">
+          <motion.h2
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            Fashion
+          </motion.h2>
+          {coverMessage.map((el, i) => (
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{
+                duration: 0.25,
+                delay: i / 10,
+              }}
+              key={i}
+            >
+              {el}{" "}
+            </motion.span>
+          ))}
+        </div>
+        <motion.span
+          animate={{
+            y: [0, 10, 0],
+            transition: {
+              duration: 1,
+              repeat: Infinity,
+            },
+          }}
+        >
+          <FaAnglesDown />
+        </motion.span>
+      </article>
+
+      <hr
+        style={{
+          backgroundColor: "rgba(0,0,0,0.1)",
+          border: "none",
+          height: "1px",
+        }}
+      />
+
+      <article className="our-services">
+        <ul>
+          {services.map((service, i) => (
+            <motion.li
+              initial={{ opacity: 0, y: -100 }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delay: i / 20,
+                },
+              }}
+              key={service.title}
+            >
+              <div>{service.icon}</div>
+              <section>
+                <h3>{service.title}Y</h3>
+                <p>{service.title}</p>
+              </section>
+            </motion.li>
+          ))}
+        </ul>
+      </article>
+    </>
+  );
+};
+
+export default Home;
